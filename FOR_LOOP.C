bb17dc75b042b53f7cce0161dbfdf82ad6a1c968
angad <angadreact01@gmail.com>
2021-12-21 10:31:31 -0700

    FIRST

--- FOR_LOOP.C
+++ FOR_LOOP.C
@@ -1,10 +1,12 @@
 // FOR LOOP
 #include <stdio.h>
 
-int main(){
-    int a=0;
-   
-    for(a=0;a<10;a++){
+int main()
+{
+    int a = 0;
+
+    for (a = 0; a < 10; a++)
+    {
         printf(" the value of a is %d\n", a);
     }
     return 0;
